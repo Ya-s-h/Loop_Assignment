244619cf350e39e46f325258d07e41848bd66b8a
Yash Aggarwal <vasu2013agg@gmail.com>
2023-11-14 00:07:39 +0530

    Remove record_value as a parameter as it is not required for further operations

--- utils/database_operations.py
+++ utils/database_operations.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytz
 from collections import defaultdict
 
+
 def utc_to_local(utc_datetime_str, local_timezone_str):
     utc_datetime = datetime.strptime(utc_datetime_str, "%Y-%m-%d %H:%M:%S.%f %Z")
     utc_timezone = pytz.utc
@@ -14,15 +15,14 @@ def utc_to_local(utc_datetime_str, local_timezone_str):
     converted_datetime = utc_timezone.localize(utc_datetime).astimezone(desired_timezone)
 
     return converted_datetime
-    
-def read_csv(file_path, key_column=None, record_value=None):
+
+def read_csv(file_path, key_column=None):
     """
     Read CSV data from the specified file.
 
     Args:
         file_path (str): The path to the CSV file.
         key_column (str, optional): The column to use as keys in the dictionary.
-        record_value (str, optional): The value to use for retrieving a specific record.
 
     Returns:
         pd.DataFrame or None: If key_column is provided, a DataFrame with filtered data.
@@ -33,10 +33,6 @@ def read_csv(file_path, key_column=None, record_value=None):
         data = pd.read_csv(file_path)
 
         if key_column:
-            if record_value:
-                # Returning all occurrences of record_value in key_column
-                return data[data[key_column] == record_value]
-            else:
                 # Returning DataFrame grouped by specific key_column
                 return data.groupby(key_column).apply(lambda x: x).reset_index(drop=True)
         else:
@@ -50,10 +46,7 @@ def read_csv(file_path, key_column=None, record_value=None):
     return pd.DataFrame() if key_column else pd.DataFrame()
 
 
-
 # Example usage:
 store_id_to_check = 1371971221718848145
-
-
 
 # print(data_initialization()["status_data"][0])
